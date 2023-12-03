048193eb15b2e5781e70cd8994b86b2e0d8df375
Mbah Shondong <l4nangproduction@gmail.com>
2023-12-03 22:51:25 +0700

    debian.sh

--- debian.sh
+++ debian.sh
@@ -3,21 +3,14 @@ echo "Download dan Install NGROK "
 wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo "Install Desktop Environment"
 sudo apt install xfce4 xfce4-terminal xorg dbus-x11 x11-xserver-utils -y
-clear
-echo "Update Repository"
 sudo apt update > /dev/null 2>&1
-echo "Menginstal Browser Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-echo "Menginstal Notepad"
 sudo apt install gedit > /dev/null 2>&1
-echo "Menginstal Media Player"
 sudo apt install -y vlc > /dev/null 2>&1
-echo "Install XRDP"
 sudo apt install xrdp -y > /dev/null 2>&1
 sudo adduser xrdp ssl-cert > /dev/null 2>&1
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-echo "XRDP RUNNING ON"
 sudo service xrdp restart > /dev/null 2>&1
 clear
 echo "----------------------------------------------------------------------"
